6c16add848c2fa17efa56cec6dba769aa41f2b3e
silas.dias <silasdjd@gmail.com>
2021-06-25 13:24:09 -0300

    Entidades e Repositorios

--- src/main/java/com/projeto/pessoasApiRest/entity/Person.java
+++ src/main/java/com/projeto/pessoasApiRest/entity/Person.java
@@ -1,4 +1,37 @@
 package com.projeto.pessoasApiRest.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity // Declara a classe como uma entidade
+@Data // Getters e Setters
+@Builder // Construçao dos Objetos
+@AllArgsConstructor //Construtores
+@NoArgsConstructor //Construtores
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Delega que o banco de dados que vai gerar o id indentando
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true) // Unique true vai permitir cadastrar cpfs unicoss
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}) // fetch esta relacionado a performece lazy deixa ocioso
+    @Column(nullable = false)
+    private List<Phone> phones;
 }
--- src/main/java/com/projeto/pessoasApiRest/entity/Phone.java
+++ src/main/java/com/projeto/pessoasApiRest/entity/Phone.java
@@ -1,4 +1,28 @@
 package com.projeto.pessoasApiRest.entity;
 
+import com.projeto.pessoasApiRest.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity // Declara a classe como uma entidade
+@Data // Getters e Setters
+@Builder // Construçao de objetos
+@AllArgsConstructor // Construtores
+@NoArgsConstructor
 public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Delega que o banco de dados que vai gerar o id
+    private Long id;
+
+    @Enumerated(EnumType.STRING)// Estamos declarando que e um enum do tipo String
+    @Column(nullable = false) // Força o banco de dados criar a coluna notNull
+    private PhoneType type;
+
+    @Column(nullable = false) // Força o banco de dados criar a coluna notNull
+    private String number;
 }
